babc5f0f24137edf703493575dc717c6212f0529
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-06-02 21:01:55 -0300

    cambios back deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,15 +3,24 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { platform } = require("os");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST , DB_DEPLOY} = process.env;
 
+const sequelize = new Sequelize(
+  DB_DEPLOY,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
+/*
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
+);*/
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
